534c27a2cb6381b8ea9f77be019e032a0987e06a
Lwz <1582855260@qq.com>
2022-06-15 19:38:19 +0800

    HysitxTest.java->Date

--- src/main/java/com/lwc/admin/Hystrix/HysitxTest.java
+++ src/main/java/com/lwc/admin/Hystrix/HysitxTest.java
@@ -1,10 +1,12 @@
 package com.lwc.admin.Hystrix;
 
 
-import com.lwc.admin.bean.Admin;
+
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 
 @Service
@@ -18,6 +20,6 @@ public class HysitxTest implements IRemoteCallService {
     }
 
     public static void main(String[] args) {
-        Admin admin = new Admin();
+        Date date = new Date();
     }
 }
